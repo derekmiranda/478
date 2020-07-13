8a2b03e74a53bd4c9d4acb4770fffb77a4bb0853
Derek Miranda <derek.miranda.1992@gmail.com>
2020-07-12 22:05:22 -0700

    Fixes easing and pulse changes

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import qs, {
 
 import poem from './poem.json'
 
-const MIN_SCALE = 0.8
-const MAX_SCALE = 1
-const IN_TIME = 1 // 4
-const HOLD_TIME = 1 // 7
-const OUT_TIME = 1 // 8
-// const EASE
+const MIN_SCALE = 1
+const MAX_SCALE = 1.3
+const IN_TIME = 4
+const HOLD_TIME = 7
+const OUT_TIME = 8
+const EASE = "power1.inOut"
 
 const container = document.getElementById('container')
 const lineEl = document.getElementById('line')
@@ -18,10 +18,14 @@ const debugEl = document.querySelector('.debug')
 
 let _currLine = 0
 let _canProgress = true
-let _queryParams, _currPulseTl
+let _queryParams, _currPulseTl, _currPulseFreq
 
 function pulseAnim(pulseFreq) {
-  function setNewPulse() {
+  if (_currPulseTl) {
+    // change curr tweens' duration
+    _currPulseTl.timeScale(_currPulseFreq / pulseFreq)
+    _currPulseFreq = pulseFreq
+  } else {
     _currPulseTl = gsap.timeline({
       repeat: -1,
       yoyo: true
@@ -31,19 +35,22 @@ function pulseAnim(pulseFreq) {
     }, {
       scale: MAX_SCALE,
       duration: pulseFreq,
-      ease: 'none'
+      ease: EASE
+    }).fromTo(container, {
+      scale: MAX_SCALE
+    }, {
+      scale: MIN_SCALE,
+      duration: pulseFreq,
+      ease: EASE
     })
+    _currPulseFreq = pulseFreq
   }
-
-  // wrap up curr pulse anim if any
-  endAnyPulse(() => {
-    setNewPulse()
-  })
 }
 
 function endAnyPulse(onComplete) {
   if (_currPulseTl) {
     _currPulseTl.repeat(0)
+    _currPulseTl.yoyo(false)
     _currPulseTl.eventCallback('onComplete', () => {
       _currPulseTl.kill()
       _currPulseTl = null
@@ -92,27 +99,28 @@ const anims = {
     })
   },
   out() {
+    endAnyPulse
+    disableBtn()
     gsap.fromTo(container, OUT_TIME, {
       scale: MAX_SCALE
     }, {
-      scale: MIN_SCALE
+      scale: MIN_SCALE,
+      onComplete() {
+        enableBtn()
+      }
     })
   },
   pulse1() {
     pulseAnim(4)
-    // pulseAnim(1)
   },
   pulse2() {
     pulseAnim(2)
-    // pulseAnim(1)
   },
   pulse3() {
     pulseAnim(1)
-    // pulseAnim(1)
   },
   pulse4() {
     pulseAnim(0.3)
-    // pulseAnim(1)
   },
   end() {
     removeContinue()
@@ -125,7 +133,7 @@ function removeContinue() {
 
 function processLine() {
   const lineData = poem[_currLine]
-  lineEl.innerText = lineData.text
+  lineEl.innerHTML = lineData.text
 
   if (lineData.anim) {
     anims[lineData.anim]()
